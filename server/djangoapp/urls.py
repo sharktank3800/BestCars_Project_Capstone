2a6ee0a7a32174dfb995e90ddb0992c676443c90
sharktank3800 <sharktank38000@gmail.com>
2024-08-09 12:22:42 -0400

    implemented django view to post a dealer review, to get dealers, function created to interact with backend created django proxy services of backend APIs

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
+from .restapis import get_request, analyze_review_sentiments, post_review
 
 app_name = 'djangoapp'
 urlpatterns = [
@@ -12,6 +13,11 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
 
     # path for dealer reviews view
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+
+    # path for get dealers
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
 
     # path for get cars
     path(route='get_cars', view=views.get_cars, name ='getcars'),
@@ -20,5 +26,9 @@ urlpatterns = [
     path(route='logout', view=views.logout, name='logout'),
 
     # path for add a review view
+    path(route='add_review', view=views.add_review, name='add_review'),
+
+    # path for get dealer details
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
